99e98214953a36c5d0af8bc470d62679844aef5d
alessandrobriggi <alessandro.briggi@yahoo.com>
2022-03-07 09:41:49 +0100

    Corretto esercizio con Spring del capitolo 4: Comment non doveva essere gestita dal context

--- chapter4/commentservice-spring/src/main/java/it/comment/Main.java
+++ chapter4/commentservice-spring/src/main/java/it/comment/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
-		Comment comment = context.getBean(Comment.class);
+		Comment comment = new Comment("This task is too ambiguous");
 		CommentService service =context.getBean(CommentService.class);
 		service.publishComment(comment);
 		context.close();
--- chapter4/commentservice-spring/src/main/java/it/comment/ProjectConfiguration.java
+++ chapter4/commentservice-spring/src/main/java/it/comment/ProjectConfiguration.java
@@ -1,18 +1,10 @@
 package it.comment;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import it.comment.model.Comment;
 
 @Configuration
 @ComponentScan(basePackages = { "it.comment" })
 public class ProjectConfiguration {
-	
-	@Bean
-	public Comment comment() {
-		return new Comment("This task is too ambiguous...");
-	}
 
 }
